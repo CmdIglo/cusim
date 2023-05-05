35654e63ad6e5ed2c92efdd529de2a911d57c7f0
CmdIglo <maxwell.leu@web.de>
2023-05-05 22:04:05 +0200

    added user input

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 
 //template code snippet
-int main() {
+int main(int argc, char** argv) {
+
+    //the cpu command
+    char command[8];
+
+    //get user input; 8-Bit cpu command
+    printf("Type in a 8Bit cpu command: \n");
+    scanf("%s", command);
+    printf("%s\n", command);
+
     return 0;
 }
